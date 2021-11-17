38e09ce97dec0af9ebf14d8a1d5782fff6574420
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-16 18:40:32 -0600

    changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,31 +4,7 @@
       <h1 class="text-3xl font-thin tracking-tighter">
         <span class="font-normal">API</span>
       </h1>
-      <nav>
-        <ul class="flex space-x-4">
-          <router-link to="/">
-            <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
-            >
-              Home
-            </li>
-          </router-link>
-          <router-link :to="{ name: 'About' }">
-            <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
-            >
-              About
-            </li>
-          </router-link>
-          <router-link :to="{ path: '/hello', name: 'Hello' }">
-            <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-green-500 hover:text-green-800"
-            >
-              Hello
-            </li>
-          </router-link>
-        </ul>
-      </nav>
+      
     </div>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Index from "./pages/index.vue";
-import About from "./pages/about.vue";
-import Hello from "./pages/hello.vue";
-import NotFound from "./pages/404.vue";
+iimport NotFound from "./pages/404.vue";
 
 const routes = [
   {
     path: "/",
     name: "Index",
     component: Index,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/hello",
-    name: "Hello",
-    component: Hello,
   },
   {
     path: "/:pathMatch(.*)*",
